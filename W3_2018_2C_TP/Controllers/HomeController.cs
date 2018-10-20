7c1ff1ebedb084cdc4d1bdaa6aa35ab963108e33
Angela Mansilla <DaniMansilla94@outlook.com>
2018-10-20 02:53:32 -0300

    Se le le agregan parte del servicio de pedido, y parte del controlador
    Faltan las vistas, pero me enfoque en el backend

--- W3_2018_2C_TP/Controllers/HomeController.cs
+++ W3_2018_2C_TP/Controllers/HomeController.cs
@@ -8,16 +8,17 @@ namespace W3_2018_2C_TP.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
-
+        [HttpGet]
         public ActionResult Login()
         { 
             return View();
         }
-
+        [HttpGet]
         public ActionResult Error()
         {
             return View();
